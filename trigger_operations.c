5875f3fd82b60752cc1660e39c2484eba3e9b224
Eddy <eddyukpong@gmail.com>
2023-12-13 12:47:29 -0800

    Read file and call function.

---
+++ trigger_operations.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * addData_to_stack1 - Adds a new node to the stack.
+ * @node: Pointer to the new node.
+ * @line_number: Current line.
+ * Return: Void.
+ */
+
+void addData_to_stack1(stack_t **node, __atttribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+}
